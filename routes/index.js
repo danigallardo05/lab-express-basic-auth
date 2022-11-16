1d2cdd0c22b6a86d9992739684508fd598ba2928
Daniela Gallardo <promillennials@gmail.com>
2022-11-16 10:16:23 -0500

    first commit

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,44 @@
 const router = require("express").Router();
+const User = require('../models/User.model');
+const bcryptjs = require('bcryptjs');
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+router.get('/signUp', (req, res, next) => {
+  res.render('signUp.hbs');
+})
+
+router.post('/signUp', (req, res, next) => {
+
+  if (!req.body.username || !req.body.password) {
+    res.send('Sorry you forgot a user name or password')
+    return;
+  }
+
+  User.findOne({ username: req.body.username })
+    .then(findUser => {
+      if (findUser) {
+        res.send('user already exists')
+        return;
+      }
+      return User.create({
+        username: req.body.username,
+        password: bcryptjs.hashSync(req.body.password)
+      })
+    })
+
+    .then(createdUser => {
+      console.log('here is the new user', createdUser);
+      res.send(createdUser);
+    })
+
+    .catch(err => {
+      res.send(err)
+    })
+
+})
 
 module.exports = router;
